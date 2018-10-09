977325d48348dcaa10ce9c71ab52b3ec94018972
simon <simon@skiscool.com>
2018-10-09 22:11:28 +0200

    commit

--- react/src/components/user/auth/ValidateEmail.js
+++ react/src/components/user/auth/ValidateEmail.js
@@ -1,61 +1,64 @@
-import React, { Component } from 'react'
-import { graphql, compose } from 'react-apollo'
-import gql from 'graphql-tag'
-import SnackBarCustom from '../../nav/SnackBarCustom'
-import Paper from '@material-ui/core/Paper'
-import { withApollo } from 'react-apollo'
+import React, { Component } from "react";
+import { graphql, compose } from "react-apollo";
+import gql from "graphql-tag";
+import Card from "../../../reactLIB/Card";
+import { withApollo } from "react-apollo";
 
-const queryString = require('query-string')
+const queryString = require("query-string");
 
 class ValidateEmail extends Component {
   state = {
-    email: '',
-    validateEmailToken: '',
-  }
+    email: "",
+    validateEmailToken: ""
+  };
 
+  notifyAboutComment(comment) {
+    var toastHTML =
+      "<span>" +
+      comment +
+      "</span><button class='btn-flat toast-action'>Undo</button>";
+    typeof M !== "undefined" && M.toast({ html: toastHTML });
+  }
+  
   componentDidMount() {
-    let validateEmailToken = queryString.parse(this.props.location.search).validateEmailToken
-    if(validateEmailToken) {
-      this.validateEmailMutation(validateEmailToken)
+    let validateEmailToken = queryString.parse(this.props.location.search)
+      .validateEmailToken;
+    if (validateEmailToken) {
+      this.validateEmailMutation(validateEmailToken);
     }
   }
 
   render() {
     return (
-      <div className='paperOut'>
-        <Paper className='paperIn'>
-        <h4 className='mv3'>
-          Email Validation
-        </h4>
-        <div className='flex flex-column'>
-
-        </div>
-
-        <SnackBarCustom ref={instance => { this.child = instance }}/>
-      </Paper>
+      <div className="paperOut">
+        <Card className="paperIn">
+          <h4 className="mv3">Email Validation</h4>
+          <div className="flex flex-column" />
+        </Card>
       </div>
-    )
+    );
   }
-  validateEmailMutation = async (validateEmailToken) => {
-    let messageSnackBar
-    await this.props.validateEmailMutation({
-      variables: {
-        validateEmailToken
-      },
-    })
-    .then((result) => {
-      const { token, user } = result.data.validateEmail
-      this._saveUserData(token, user)
-      messageSnackBar = `${user.name}, your email is now validated.`
-    })
-    .catch((e) => { messageSnackBar = e.graphQLErrors[0].message })
-    this.child._openSnackBar(messageSnackBar)
-  }
+  validateEmailMutation = async validateEmailToken => {
+    await this.props
+      .validateEmailMutation({
+        variables: {
+          validateEmailToken
+        }
+      })
+      .then(result => {
+        const { token, user } = result.data.validateEmail;
+        this.notifyAboutComment(`${user.name}, your email is now validated.`);
+
+        this._saveUserData(token, user);
+      })
+      .catch(e => {
+        this.notifyAboutComment(e.graphQLErrors[0].message);
+      });
+  };
 
   _saveUserData = () => {
-    this.props.client.resetStore().then( ()=> {
-    })
-  }
+    this.props.client.resetStore().then(() => {});
+  };
 }
 
 const VALIDATE_EMAIL_TOKEN_MUTATION = gql`
@@ -69,9 +72,9 @@ const VALIDATE_EMAIL_TOKEN_MUTATION = gql`
       }
     }
   }
-`
+`;
 
 export default compose(
-  graphql(VALIDATE_EMAIL_TOKEN_MUTATION, { name: 'validateEmailMutation' }),
+  graphql(VALIDATE_EMAIL_TOKEN_MUTATION, { name: "validateEmailMutation" }),
   withApollo
-)(ValidateEmail)
+)(ValidateEmail);
